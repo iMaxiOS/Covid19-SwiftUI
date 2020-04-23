46c9db71fa1fce4a08c08b9156ff85c6740c57bd
Macostik <y.granchenko@gmail.com>
2020-04-23 18:52:33 +0300

    add project

---
+++ Covid19/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Covid19
+//
+//  Created by Maxim Granchenko on 23.04.2020.
+//  Copyright © 2020 Maxim Granchenko. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
